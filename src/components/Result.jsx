5a2633d76abf8f9074e86204c0d23ab7b531f3db
simokitkat <simokitkat@yahoo.com>
2023-10-20 13:36:53 +0300

    feat: add styles to the Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,23 +2,27 @@
 export default function Result({ tipAmount, total }) {
   return (
     <section className="result">
-      <div className="tip-amount">
-        <div className="text">
-          <h3>Tip Amount</h3>
-          <span>/ person</span>
+      <div className="container">
+        <div className="top">
+          <div className="tip-amount">
+            <div className="text">
+              <h3>Tip Amount</h3>
+              <span>/ person</span>
+            </div>
+            <span className="amount-span">${tipAmount}</span>
+          </div>
+          <div className="result-amount">
+            <div className="text">
+              <h3>Total</h3>
+              <span>/ person</span>
+            </div>
+            <span className="result-span">${total}</span>
+          </div>
         </div>
-        <span className="amount-span">${tipAmount}</span>
-      </div>
-      <div className="result-amount">
-        <div className="text">
-          <h3>Total</h3>
-          <span>/ person</span>
+        <div className="buttons">
+          <button type="submit">Submit</button>
+          <button type="reset">Reset</button>
         </div>
-        <span className="result-span">${total}</span>
-      </div>
-      <div className="buttons">
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
       </div>
     </section>
   );
